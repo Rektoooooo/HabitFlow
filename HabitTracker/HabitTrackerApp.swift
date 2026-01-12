145da29d52d5f2637fed8db011aa4426a3da5e35
Rektoooooo <sebastian.kucera@icloud.com>
2026-01-12 13:16:10 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Sebastián Kučera on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
